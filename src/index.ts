ff2ea636c38cd61054b692f8b74bf965be712585
jarkurghan <jarkurghan@gmail.com>
2026-03-15 16:06:05 +0500

    hono

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { Hono } from "hono";
+// import { handleUpdate } from "./bot";
+
+const app = new Hono();
+
+// app.post("/bot", async (c) => await handleUpdate(c));
+app.get("/", (c) => c.text("Hello Hono!"));
+
+export default app;
+
+// import { startBot } from "./bot";
+
+// startBot();
